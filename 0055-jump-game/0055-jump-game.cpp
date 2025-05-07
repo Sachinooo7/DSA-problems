cc7e727ae40afab9befa900b47fd942a17670117
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-05-07 23:15:58 +0530

    Time: 0 ms (100%), Space: 52.4 MB (19.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-// void solve(vector<int>& nums,int i,bool &b,int n){
-//     if(i==n) {
-//         b=1;return;
-//     }
-//     for(int x=nums[i];x>=0;x--){
-//         solve(nums,i+x,b,n);
-//     }
-// }
+
     bool canJump(vector<int>& nums) {
-        // bool b=0;
         int n=nums.size();
-        // solve(nums,0,b,n-1);
-        // return b;
+        
 
         int mx=0;
         for(int i=0;i<n;i++){
