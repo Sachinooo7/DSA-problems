234bb95e89ab4ebc93b05e04ba824755a2bf7b91
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-07-01 10:54:21 +0530

    Time: 0 ms (100%), Space: 16.5 MB (70.89%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,45 +10,46 @@
  */
 class Solution {
 public:
-ListNode* reverseList(ListNode* head) {
-        ListNode* prev=nullptr;
-        ListNode* cur=head;
-        while(cur){
-            ListNode* temp=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=temp;
+
+void solve(ListNode* head){
+
+    ListNode* prev=nullptr;
+    ListNode* curr=head;
+
+    while(curr!=nullptr){
+        ListNode* temp=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=temp;
         }
-        return prev;
-        
-    }
+    
+}
     ListNode* reverseKGroup(ListNode* head, int k) {
-         if (!head || k == 1) return head;
 
-         ListNode* temp=head;
-            ListNode* prev=nullptr;
-         while(temp!=nullptr){
-
+        int c=0;
+        ListNode* temp=head;
+        ListNode* prev=nullptr;
+        while(temp!=nullptr){
             ListNode* kth=temp;
             for(int i=1;i<k && kth;i++){
                 kth=kth->next;
             }
             if(kth==nullptr){
-                if(prev) prev->next=temp;
-                break;
+                if(prev)
+                prev->next=temp; break;
             }
-            ListNode* nextnode=kth->next;
+            ListNode* nth=kth->next;
             kth->next=nullptr;
-            reverseList(temp);
+            solve(temp);
             if(temp==head){
                 head=kth;
             }
             else{
                 prev->next=kth;
-            }
+            } 
             prev=temp;
-            temp=nextnode;
-         }
-         return head;
+             temp=nth;
+        }
+        return head;
     }
 };
