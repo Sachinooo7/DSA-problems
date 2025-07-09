ffb310c488eba187ae34f474c5a161ed5ddcfeef
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-07-09 12:14:13 +0530

    Time: 0 ms (100%), Space: 13.2 MB (31.27%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-
-
     int minimumTotal(vector<vector<int>>& tri) {
-        int r=tri.size();
-        int c=tri[r-1].size();
-        vector<vector<int>>dp(r,vector<int>(c,-1));
 
-        for(int i=0;i<c;i++){
-            dp[r-1][i]=tri[r-1][i];        }
+        int n=tri.size();
+        int m=tri[n-1].size();
 
-            for(int i=r-2;i>=0;i--){
-                for(int j=i;j>=0;j--){
-                    int d=tri[i][j]+dp[i+1][j];
-                    int dg=tri[i][j]+dp[i+1][j+1];
-                    dp[i][j]=min(d,dg);
-                }
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
+        dp[0][0]=tri[0][0];
+
+
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<=i;j++){
+                dp[i+1][j]=min(dp[i+1][j],dp[i][j]+tri[i+1][j]);
+                dp[i+1][j+1]=min(dp[i+1][j+1],dp[i][j]+tri[i+1][j+1]);
             }
-        return dp[0][0];
+        }
+
+        int sum=INT_MAX;
+        for(int j=0;j<m;j++)
+        {
+            sum=min(sum,dp[n-1][j]);
+        }
+        return sum;
     }
 };
