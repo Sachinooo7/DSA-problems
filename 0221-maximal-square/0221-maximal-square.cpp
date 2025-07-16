a706bacac6fd26e49b17d346ba94fb9d580b460d
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-07-16 22:38:51 +0530

    Time: 7 ms (72.49%), Space: 27.5 MB (97.62%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -23,23 +23,31 @@ public:
         int maxi=0;
         int r=matrix.size();
         int c=matrix[0].size();
-        vector<vector<int>>dp(r+1,vector<int>(c+1,0));
+        // vector<vector<int>>dp(r+1,vector<int>(c+1,0));
+        vector<int>curr(r+1,0);
+        vector<int>next(r+1,0);
 
 
-        for(int i=r-1;i>=0;i--){
             for(int j=c-1;j>=0;j--){
-
-                  int right=dp[i][j+1];
-                  int dgnl=dp[i+1][j+1];
-                  int down=dp[i+1][j];
+        for(int i=r-1;i>=0;i--){
+        
+                //   int right=curr[j+1];
+                int right=next[i];
+                //   int dgnl=next[j+1];
+                int dgnl=next[i+1];
+                //   int down=next[j];
+                int down=curr[i+1];
 
                   if(matrix[i][j]=='1'){
-                     dp[i][j]=1+min(right,min(dgnl,down));
-                     maxi=max(maxi,dp[i][j]);
+                     curr[i]=1+min(right,min(dgnl,down));
+                     maxi=max(maxi,curr[i]);
                       }
+                      else curr[i]=0;
             }
+            next=curr;
         }
         // solve(matrix,0,0,maxi,dp);
+    
         return maxi*maxi;
         
     }
