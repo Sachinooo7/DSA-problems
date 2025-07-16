085014dc9132099eb9fa7b261b8f26cacb4e27b1
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-07-16 19:48:41 +0530

    Time: 11 ms (47.31%), Space: 30.8 MB (85.13%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+// int solve(vector<vector<char>>& matrix,int i,int j,int &maxi,vector<vector<int>>&dp){
+//     if(i>=matrix.size() || j>=matrix[0].size()) return 0;
+
+//     if(dp[i][j]!=-1) return dp[i][j];
+//     int right=solve(matrix,i,j+1,maxi,dp);
+//     int dgnl=solve(matrix,i+1,j+1,maxi,dp);
+//     int down=solve(matrix,i+1,j,maxi,dp);
+
+//     if(matrix[i][j]=='1'){
+//         int ans=1+min(right,min(dgnl,down));
+//         maxi=max(maxi,ans);
+//         return dp[i][j]=ans;
+//     }
+//     else {
+//     return dp[i][j]=0;
+//     }
+// }
+    int maximalSquare(vector<vector<char>>& matrix) {
+
+        int maxi=0;
+        int r=matrix.size();
+        int c=matrix[0].size();
+        vector<vector<int>>dp(r+1,vector<int>(c+1,0));
+
+
+        for(int i=r-1;i>=0;i--){
+            for(int j=c-1;j>=0;j--){
+
+                  int right=dp[i][j+1];
+                  int dgnl=dp[i+1][j+1];
+                  int down=dp[i+1][j];
+
+                  if(matrix[i][j]=='1'){
+                     dp[i][j]=1+min(right,min(dgnl,down));
+                     maxi=max(maxi,dp[i][j]);
+                      }
+            }
+        }
+        // solve(matrix,0,0,maxi,dp);
+        return maxi*maxi;
+        
+    }
+};
