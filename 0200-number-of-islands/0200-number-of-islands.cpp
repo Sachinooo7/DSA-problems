78fe80b20b55a9777e0594bc5d37e95c74725bf3
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-07-28 12:18:55 +0530

    Time: 31 ms (32.85%), Space: 23.3 MB (32.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-bool valid(int x,int y,vector<vector<char>>& grid,int n,int m){
-    if(x<0 || x>=n || y<0||y>=m || grid[x][y]=='0' || grid[x][y]=='2'){
-        return false;
-    }
-    return true;
-}
-void solve(vector<vector<char>>& grid,int i,int j,int n,int m){
+
+void solve(vector<vector<char>>& grid,int i,int j,int r,int c){
     queue<pair<int,int>>q;
     q.push({i,j});
 
@@ -14,40 +9,43 @@ void solve(vector<vector<char>>& grid,int i,int j,int n,int m){
         auto t=q.front(); q.pop();
         int x=t.first;
         int y=t.second;
+        // grid[x][y]='2';
 
-        if(valid(x+1,y,grid,n,m)){
+        if(y+1<c && grid[x][y+1]=='1'  ){
+            q.push({x,y+1});         grid[x][y+1]='2';
 
-            q.push({x+1,y});
-        grid[x+1][y]='2';
         }
-        if(valid(x,y+1,grid,n,m)){
-            q.push({x,y+1});
-            grid[x][y+1]='2';
+        if(y>0 && grid[x][y-1]=='1'){
+            q.push({x,y-1});         grid[x][y-1]='2';
+
         }
-        if(valid(x,y-1,grid,n,m)){
-            q.push({x,y-1});
-            grid[x][y-1]='2';
+        if(x+1<r && grid[x+1][y]=='1'){
+            q.push({x+1,y});         grid[x+1][y]='2';
+
         }
-        if(valid(x-1,y,grid,n,m)){
-            q.push({x-1,y});
-            grid[x-1][y]='2';
+        if(x>0 && grid[x-1][y]=='1' ){
+            q.push({x-1,y});         grid[x-1][y]='2';
+
         }
+
     }
-
-
 }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if( grid[i][j]=='1'){
-                    c++;
-                    solve(grid,i,j,n,m);
+
+        int r=grid.size();
+        int c=grid[0].size();
+
+
+        int res=0;
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]=='1'){
+                    solve(grid,i,j,r,c);
+                    res++;
                 }
             }
         }
-        return c;
+        return res;
     }
 };
