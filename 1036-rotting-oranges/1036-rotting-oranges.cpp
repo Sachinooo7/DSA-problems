fcfc6222cd7ac5ff080550f2baca5fa22fbb8844
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-08-04 12:49:18 +0530

    Time: 5 ms (9.34%), Space: 17.8 MB (8.16%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-int solve(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&vis){
+int solve(vector<vector<int>>& grid,int i,int j){
     int r=grid.size();
     int c=grid[0].size();
 
@@ -10,7 +10,7 @@ int solve(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&vis){
             for(int j=0;j<c;j++){
                 if(grid[i][j]==2){
                  q.push({i,j,0});  
-                 vis[i][j]=2; 
+                //  vis[i][j]=2; 
                 }
             }
         }
@@ -25,24 +25,20 @@ int mx=0;
         mx=max(mx,cnt);
         
 
-        if( x+1<r && grid[x+1][y]==1 && vis[x+1][y]!=2){
-            grid[x+1][y]=2;
-            vis[x+1][y]=2;
+        if( x+1<r && grid[x+1][y]==1 ){
+            grid[x+1][y]=3;
             q.push({x+1,y,cnt+1});
         }
-         if(  x-1>=0 && grid[x-1][y]==1 && vis[x-1][y]!=2){
-            grid[x-1][y]=2;
-            vis[x-1][y]=2;
+         if(  x-1>=0 && grid[x-1][y]==1 ){
+            grid[x-1][y]=3;
             q.push({x-1,y,cnt+1});
         }
-         if(  y+1<c && grid[x][y+1]==1 && vis[x][y+1]!=2 ){         
-            grid[x][y+1]=2;
-            vis[x][y+1]=2;
+         if(  y+1<c && grid[x][y+1]==1){         
+            grid[x][y+1]=3;
             q.push({x,y+1,cnt+1});
         }
-         if(  y-1>=0 && grid[x][y-1]==1 && vis[x][y-1]!=2 ){
-            grid[x][y-1]=2;
-            vis[x][y-1]=2;
+         if(  y-1>=0 && grid[x][y-1]==1){
+            grid[x][y-1]=3;
             q.push({x,y-1,cnt+1});
         }
     }
@@ -52,11 +48,11 @@ int mx=0;
          int r=grid.size();
     int c=grid[0].size();
 
-vector<vector<int>>vis(r,vector<int>(c,0));
-         int mx=solve(grid,0,0,vis);
+// vector<vector<int>>vis(r,vector<int>(c,0));
+         int mx=solve(grid,0,0);
          for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1 && vis[i][j]!=2){
+                if(grid[i][j]==1){
 return -1;
                 }
             }
