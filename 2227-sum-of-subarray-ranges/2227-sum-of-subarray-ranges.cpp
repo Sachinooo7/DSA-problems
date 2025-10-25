0ecbb1b5e8e34163f765691ee1fa9639bdac4ab4
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-10-25 23:46:19 +0530

    Time: 7 ms (44.4%), Space: 16.1 MB (16.68%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,18 +1,85 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        
+
+long long findMin(vector<int>arr){
+     stack<int>s;
+        int n=arr.size();
+        vector<int>pre(n),next(n);
+
+        for(int i=0;i<n;i++){
+
+            while(!s.empty() && arr[s.top()]>arr[i]) s.pop();
+            pre[i]=s.empty()?-1:s.top();
+            s.push(i);
+
+        }
+        s=stack<int>();
+
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!s.empty() && arr[s.top()]>=arr[i]) s.pop();
+
+            next[i]=s.empty()?n:s.top();
+            s.push(i);
+
+        }
+
         long long sum=0;
-        for(int i=0;i<nums.size();i++){
-        int mx=INT_MIN;
-        int mn=INT_MAX;
-            for(int j=i;j<nums.size();j++){
-                mx=max(mx,nums[j]);
-                mn=min(mn,nums[j]);
-                sum+=mx-mn;
-            }
-            
+
+        for(int i=0;i<n;i++){
+
+            long long t=(i-pre[i]) * (next[i]-i);
+            // sum+=t*arr[i]%mod;
+            sum += t * arr[i];
+
         }
         return sum;
+}
+
+long long findMax(vector<int>arr){
+
+     stack<int>s;
+        int n=arr.size();
+        vector<int>pre(n);
+
+        for(int i=0;i<n;i++){
+
+            while(!s.empty() && arr[s.top()]<arr[i]) s.pop();
+            pre[i]=s.empty()?-1:s.top();
+            s.push(i);
+
+        }
+        s=stack<int>();
+
+         vector<int>next(n);
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!s.empty() && arr[s.top()]<=arr[i]) s.pop();
+
+            next[i]=s.empty()?n:s.top();
+            s.push(i);
+
+        }
+
+        long long sum=0;
+
+        for(int i=0;i<n;i++){
+
+            long long t=(i-pre[i]) * (next[i]-i);
+            // sum+=t*arr[i]%mod;
+            sum += t * arr[i];
+
+        }
+        return sum;
+}
+    long long subArrayRanges(vector<int>& nums) {
+
+        long long mn=findMin(nums);
+        long long mx=findMax(nums);
+
+        return mx-mn;
+        
     }
 };
