cb4aaa2203ea1c9e6a2705d3282ea215432da82c
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2025-11-08 16:11:19 +0530

    Time: 1 ms (83.37%), Space: 42.8 MB (11.12%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {
+
+        if(s.charAt(0)=='0') return 0;
+
+        int n=s.length();
+
+       int [] dp=new int[n+1];
+        dp[0]=dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+
+            int fir=Character.getNumericValue(s.charAt(i-1));
+            int sec=Integer.parseInt(s.substring(i-2,i));
+
+            if(fir>0 && fir<=9) dp[i]+=dp[i-1];
+            if(sec>=10 && sec<=26) dp[i]+=dp[i-2];
+
+        }
+
+
+return dp[n];
+
+    }
+}
