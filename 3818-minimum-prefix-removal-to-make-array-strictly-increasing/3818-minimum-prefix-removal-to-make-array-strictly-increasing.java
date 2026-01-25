c37d9c841118752917da8d4194b942259c3504f7
Sachinooo7 <160403102+Sachinooo7@users.noreply.github.com>
2026-01-25 15:00:08 +0530

    Time: 1 ms (99.26%), Space: 120.4 MB (40.15%) - LeetHub

---
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumPrefixLength(int[] nums) {
+
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i]<=nums[i-1]){
+                return i;
+            }
+        }
+        return 0;
+        
+    }
+}
