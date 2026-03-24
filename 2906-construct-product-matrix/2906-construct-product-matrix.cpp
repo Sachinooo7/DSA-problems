089a979045f00701145c56ce3a800c5b60bc1c0f
sachin dadhwal <160403102+Sachinooo7@users.noreply.github.com>
2026-03-24 12:52:44 +0530

    Time: 61 ms (27.68%), Space: 147.4 MB (30.63%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
 
         long long mul=1;
+        int mod=12345;
         int r=grid.size();
         int c=grid[0].size();
 
         vector<vector<int>>first(r,vector<int>(c));
-        vector<vector<int>>second(r,vector<int>(c));
-
+ 
 
         int fir=1;
         int sec=1;
@@ -24,19 +24,17 @@ public:
 
         for(int i=r-1;i>=0;i--){
             for(int j=c-1;j>=0;j--){
-                second[i][j] = ( (long long)fir * sec ) % 12345;
-                fir=second[i][j];
-                sec=grid[i][j];
+
+                 int curr = grid[i][j]; // ✅ store original
+
+                grid[i][j] = ((long long)first[i][j] * fir) % mod;
+
+                fir = ((long long)fir * curr) % mod; // ✅ use original
 
             }
         }
 
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-
-                 grid[i][j] = ( (long long)first[i][j] * second[i][j] ) % 12345;
-            }
-        }
+        
 
 
         return grid;
